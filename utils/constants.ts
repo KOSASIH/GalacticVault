4986ab16bed918d7eccf3d7e0f21c835b4f3ca2e
KOSASIH <kosasihg88@gmail.com>
2024-06-15 16:02:37 +0700

    Create constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,35 @@
+// utils/constants.ts
+export const APP_NAME = 'OmniFusion';
+export const APP_VERSION = '1.0.0';
+export const API_URL = 'https://api.omnifusion.io';
+export const API_KEY = 'YOUR_API_KEY_HERE';
+
+export const CURRENCY_CODES = {
+  USD: 'USD',
+  EUR: 'EUR',
+  BTC: 'BTC',
+  ETH: 'ETH',
+};
+
+export const TRANSACTION_TYPES = {
+  DEPOSIT: 'deposit',
+  WITHDRAWAL: 'withdrawal',
+  TRANSFER: 'transfer',
+};
+
+export const BANKING_SYSTEM_TYPES = {
+  PERSONAL: 'personal',
+  BUSINESS: 'business',
+};
+
+export const CRYPTO_GATEWAY_TYPES = {
+  BITCOIN: 'bitcoin',
+  ETHEREUM: 'ethereum',
+  LITECOIN: 'litecoin',
+};
+
+export const OMNI_FUSION_TOKEN_TYPES = {
+  ERC20: 'erc20',
+  ERC721: 'erc721',
+  BEP20: 'bep20',
+};
