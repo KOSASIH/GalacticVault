a67fa65f48ebad80b243ddbfe261403f90a93f12
KOSASIH <kosasihg88@gmail.com>
2024-06-16 19:12:05 +0700

    Create App.js

---
+++ containers/App.js
@@ -0,0 +1,17 @@
+import React from 'eact';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <!-- Add content here -->
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
