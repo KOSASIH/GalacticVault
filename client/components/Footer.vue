67a6110c4e4e625598534c4fc280cebbee30d84c
KOSASIH <kosasihg88@gmail.com>
2024-06-16 22:29:06 +0700

    Create Footer.vue

---
+++ client/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <div class="container">
+      <p>&copy; 2024 Galactic Vault. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
+</style>
