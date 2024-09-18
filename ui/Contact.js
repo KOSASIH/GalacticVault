2ba6ca5f5070daffacee7a1cf3842a485e9033b9
KOSASIH <kosasihg88@gmail.com>
2024-09-18 14:08:16 +0700

    Create Contact.js

---
+++ ui/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+};
+
+export default Contact;
